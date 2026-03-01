c1bbc114aedafe114f8b8ec19c57dbdfa5d07cd3
Pritika-Shukla <pritikashukla21@gmail.com>
2026-03-01 21:21:49 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ function getBaseUrl(): string {
   if (process.env.NEXT_PUBLIC_APP_URL) {
     return process.env.NEXT_PUBLIC_APP_URL.replace(/\/$/, "");
   }
- 
+  return "http://localhost:3000";
 }
 
 export default async function DashboardPage() {
